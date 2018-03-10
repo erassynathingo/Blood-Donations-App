069d5b50bbaf27e9cf7021277b3435569fce304d
Erastus Nathingo <contact@erassy.com>
2018-03-10 15:50:50 +0200

    right alligned button

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -85,7 +85,9 @@ export class LoginComponent implements OnInit, AfterViewInit {
           this.appComponent.activateUser();
           this.userService.getUser().then(data => this.User = data);
           this.router.navigate(['/dashboard']);
-          //location.reload();
+          setTimeout(() => {
+            location.reload();
+          }, 1000);
           localStorage.setItem('role', user.role);
           $(".fixed.menu").transition("horizontal flip");
         }, 3000);
